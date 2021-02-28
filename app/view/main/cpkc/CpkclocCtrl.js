c9f3552afd6a2919dc3aab5427e6663f82affc20
fslinfy <45396920+fslinfy@users.noreply.github.com>
2021-02-28 21:05:06 +0800

    u

--- app/view/main/cpkc/CpkclocCtrl.js
+++ app/view/main/cpkc/CpkclocCtrl.js
@@ -114,6 +114,10 @@ Ext.define('MyApp.view.main.cpkc.CpkclocCtrl', {
             "#btnQueryCpmc": {
                 click: that.SelectCpbmView
             },
+            "#btnPrint": {
+                click: this.onBtnPrintClick
+            },
+
             "#btnExport": {
                 click: that.onBtnExportClick
             },
@@ -124,6 +128,10 @@ Ext.define('MyApp.view.main.cpkc.CpkclocCtrl', {
         var store = that.viewname.getStore();
         var tool = that.getView().down("#QueryToolbarView");
         tool.down('#btnExport').setHidden(false);
+        tool.down('#btnPrint').setHidden(false);
+        tool.down('#btnPrint').setText('导出总表');
+
+
     },
     onBtnExportClick: function (record) {
         var that = this;
@@ -158,13 +166,181 @@ Ext.define('MyApp.view.main.cpkc.CpkclocCtrl', {
         }
         var prtData = {
             "options": {
-                "fileName": "商品仓位明细库存表"
+                "fileName": "商品库存表"
             },
             "tableData": tableDataarr
         }
         Jhxlsx.export(prtData.tableData, prtData.options);
         return;
     },
+
+    onBtnPrintClick: function (record) {
+        var that=this;
+        //var area = that.viewname.getViewModel().get('area');
+        
+        var title='商品库存总表';
+        
+        var store = that.getView().getStore();
+            var kcarray = store.data.items;
+            var sheetarr = [];
+            var khid=0;
+            for (var i = 0; i < kcarray.length; i++) {
+                var oldobj = kcarray[i].data;
+                if (oldobj.khid!=khid){
+                    khid=oldobj.khid;
+                    sheetarr.push(
+                   {
+                    "khid":khid,
+                    "khjc":oldobj.khjc  ,
+                    "khmc":oldobj.khmc  
+                   }
+                );
+                }
+            } 
+            var tableDataarr=[];
+            var jsonSheetData=[];
+                 jsonSheetData=this.getexcelsheetdata1(kcarray,title) ;
+                tableDataarr.push(
+                  {
+                    "sheetName":title,
+                    "data": jsonSheetData
+                  }
+                );
+
+            var prtData = {
+                "options": {
+                    "fileName": title
+                },
+                "tableData":tableDataarr
+            }
+            Jhxlsx.export(prtData.tableData, prtData.options);
+            return ;
+        },
+        getexcelsheetdata1: function (kcarray,title) {
+            var that=this;
+                khid=0;
+            var jsonData=[];
+            
+    
+            
+            var arr ;
+            
+            for (var i = 0; i < kcarray.length; i++) {
+                var oldobj = kcarray[i].data;
+                if (oldobj.khid!=khid){
+                 var sumsl=0;
+                 var sumzl=0;
+                 if (khid>0){
+                 jsonData.push([]);
+                 jsonData.push([]);
+                 jsonData.push([]);
+                 }
+            arr = [
+                {
+                    "merge": {
+                        "c": 10
+                    },
+                    "style": {
+                        "fill":{
+                            "bgcolor":"red"
+                        },
+                        "font": {
+                            "size":36,
+                            "bold": true
+                        }
+                    },
+                    "text": "客户：" + oldobj.khmc
+                }
+    
+            ]
+            jsonData.push(arr);  //增加小标题
+            
+            arr = [{
+                'text': '    产地       ', "style": {
+                    
+                    "font": {
+                        "bold": true
+                    },
+                    "color": {
+                        "rgb": 'FF4F81BD' 
+                      }
+                }
+            }, {    
+                'text': '     商品名称     ', 
+                   "style": {
+                    "font": {
+                        "bold": true
+                    },
+                    "width": 500,
+                    "backgcolor":"blue"
+                }
+            }, {
+                'text': '      包装     ', "style": {
+                    "font": {
+                        "bold": true
+                    }
+                }
+            }, {
+                'text': '      规格   ', "style": {
+                    "font": {
+                        "bold": true
+                    }
+                }
+            }, {
+                'text': '批号     ', "style": {
+                    "font": {
+                        "bold": true
+                    }
+                }
+            }, 
+             {
+                'text': '  单位  ', "style": {
+                    "font": {
+                        "bold": true
+                    }
+                }
+            }, {
+                'text': '库存数量   ', "style": {
+                    "font": {
+                        "bold": true
+                    }
+                }
+            }, {
+                'text': '库存重量    ', "style": {
+                    "font": {
+                        "bold": true
+                    }
+                }
+            }
+            ];
+            jsonData.push(arr);
+            
+    
+            }
+            
+                arr = [];
+                
+              
+              //  var oldobj = kcarray[i].data;
+                arr.push({ 'text': oldobj.cdmc });
+                arr.push({ 'text': oldobj.cpmc });
+                arr.push({ 'text': oldobj.bzmc });
+                arr.push({ 'text': oldobj.cpgg });
+                arr.push({ 'text': oldobj.cpph });
+                
+                arr.push({ 'text': oldobj.jldw });
+                arr.push({ 'text': slrenderer(oldobj.sl) });
+                arr.push({ 'text': slrenderer(oldobj.zl) });
+                sumsl+=oldobj.sl;
+                sumzl+=oldobj.zl;
+                jsonData.push(arr);
+              
+           khid=oldobj.khid;
+    
+        }
+            return   jsonData;
+        },
+    
     getexcelsheetdata: function (kcarray, khid, khmc) {
         var that = this;
         var jsonData = [];
@@ -185,7 +361,7 @@ Ext.define('MyApp.view.main.cpkc.CpkclocCtrl', {
                         "horizontal": 'center'
                     }
                 },
-                "text": "商品仓位明细库存表"
+                "text": "商品库存表"
             }
         ]
         jsonData.push(arr);  //增加标题
